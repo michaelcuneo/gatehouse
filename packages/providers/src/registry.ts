dd741984288d94445b91dfe6483d83698938688d
Michael Cuneo <michaelcuneo75@gmail.com>
2026-09-24 09:32:55 +1000

    feat(providers): register systemd service provider

--- packages/providers/src/registry.ts
+++ packages/providers/src/registry.ts
@@ -1,9 +1,11 @@
 import { filesystemProvider } from "./filesystem";
 import { nginxProvider } from "./nginx";
+import { systemdProvider } from "./systemd";
 
 export const providers = {
   nginx: nginxProvider,
   filesystem: filesystemProvider,
+  systemd: systemdProvider,
 };
 
 export function getProvider(name: string) {
