52ea0f4c07ac73a52adfe39394c4ddba58e68e05
Michael Cuneo <michaelcuneo75@gmail.com>
2026-09-24 09:29:20 +1000

    feat(reconciliation): export dependency planner

--- packages/reconciliation/src/index.ts
+++ packages/reconciliation/src/index.ts
@@ -1,6 +1,5 @@
 export * from "./types";
-
 export * from "./resolveProvider";
-
+export * from "./planReconciliation";
 export * from "./reconcileResource";
 export * from "./reconcileAllResources";
