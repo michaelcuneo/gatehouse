4c5808ca59d3dca24d6f888b517e1542c55e4712
Michael Cuneo <michaelcuneo75@gmail.com>
2026-09-24 09:43:27 +1000

    feat(reconciliation): export provider context resolver

--- packages/reconciliation/src/index.ts
+++ packages/reconciliation/src/index.ts
@@ -4,3 +4,4 @@ export * from "./planReconciliation";
 export * from "./reconcileResource";
 export * from "./reconcileAllResources";
 export * from "./checkResourceHealth";
+export * from "./providerContext";
