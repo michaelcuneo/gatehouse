e68ca17b57de4e534658568fd49de30d3018c0ff
Michael Cuneo <michaelcuneo75@gmail.com>
2026-09-24 09:34:12 +1000

    feat(resources): audit resource updates

--- packages/resources/src/updateResource.ts
+++ packages/resources/src/updateResource.ts
@@ -1,4 +1,7 @@
-import { saveResource } from "@gatehouse/db";
+import {
+  saveResource,
+  writeAuditLog,
+} from "@gatehouse/db";
 import type { Resource } from "@gatehouse/types";
 
 import { validateResource } from "./validateResource";
@@ -7,6 +10,13 @@ export function updateResource(resource: Resource): Resource {
   validateResource(resource);
 
   saveResource<Resource["spec"]>(resource);
+
+  writeAuditLog({
+    resourceId: resource.id,
+    action: "update",
+    success: true,
+    message: `Updated ${resource.kind} resource "${resource.name}"`,
+  });
 
   return resource;
 }
