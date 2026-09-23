18d3b6ba5ac90ed92c0ded4a5cf089661bcf36d8
Michael Cuneo <michaelcuneo75@gmail.com>
2026-09-23 11:14:32 +1000

    fix(resources): export runtime state updater

--- packages/resources/src/index.ts
+++ packages/resources/src/index.ts
@@ -3,4 +3,5 @@ export * from "./updateResource";
 export * from "./deleteResource";
 export * from "./getResource";
 export * from "./listResources";
+export * from "./updateResourceRuntime";
 export * from "./validateResource";
