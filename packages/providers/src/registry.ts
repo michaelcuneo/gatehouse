d19c8d8492730201bffc0e01110587fe5f3df35c
Michael Cuneo <michaelcuneo75@gmail.com>
2026-09-24 09:59:44 +1000

    feat(providers): register Route53 provider

--- packages/providers/src/registry.ts
+++ packages/providers/src/registry.ts
@@ -1,11 +1,13 @@
 import { filesystemProvider } from "./filesystem";
 import { nginxProvider } from "./nginx";
+import { route53Provider } from "./route53";
 import { systemdProvider } from "./systemd";
 
 export const providers = {
   nginx: nginxProvider,
   filesystem: filesystemProvider,
   systemd: systemdProvider,
+  route53: route53Provider,
 };
 
 export function getProvider(name: string) {
