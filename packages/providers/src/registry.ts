c21aa5d43aa9b17722a6c657ba79e0ac57f6555a
Michael Cuneo <michaelcuneo75@gmail.com>
2026-09-24 10:47:21 +1000

    feat(providers): register S3 provider

--- packages/providers/src/registry.ts
+++ packages/providers/src/registry.ts
@@ -1,6 +1,7 @@
 import { filesystemProvider } from "./filesystem";
 import { nginxProvider } from "./nginx";
 import { route53Provider } from "./route53";
+import { s3Provider } from "./s3";
 import { systemdProvider } from "./systemd";
 
 export const providers = {
@@ -8,6 +9,7 @@ export const providers = {
   filesystem: filesystemProvider,
   systemd: systemdProvider,
   route53: route53Provider,
+  s3: s3Provider,
 };
 
 export function getProvider(name: string) {
