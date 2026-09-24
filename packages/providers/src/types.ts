a6f6b0b8ce7b7f5ef809b6fad531cb153c46e3b8
Michael Cuneo <michaelcuneo75@gmail.com>
2026-09-24 22:37:21 +1000

    feat(providers): expose dependency stage associations

--- packages/providers/src/types.ts
+++ packages/providers/src/types.ts
@@ -9,6 +9,7 @@ export interface ProviderHealth {
 export interface ProviderContext {
   projectStages: ProjectStageContext[];
   dependencies: Resource[];
+  dependencyStages: Record<string, ProjectStageContext[]>;
 }
 
 export interface Provider {
